2fadd3a0449523c5ea6b65947cc45e99d0a16133
Lasky206 <rick.allen206@gmail.com>
2020-07-06 15:40:04 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

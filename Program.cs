b9a51951c0afba4df00476263a6999ef2a5801de
AlbinoBLK3sheep <68408417+AlbinoBLK3sheep@users.noreply.github.com>
2020-07-16 19:23:38 -0700

    i changed "helo world" to "hello chase"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace WarScraper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Chase");
         }
     }
 }
